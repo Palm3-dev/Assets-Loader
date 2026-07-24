ace65cfb9f30c499086a78089821e190cfc05371
Palm3 <palmerini.2025@gmail.com>
2026-07-24 15:57:51 +0200

    Added conditional logging

--- src/main/java/com/palm3/assets_loader/assets/patchers/ObjModelsChanger.java
+++ src/main/java/com/palm3/assets_loader/assets/patchers/ObjModelsChanger.java
@@ -25,21 +25,24 @@ public class ObjModelsChanger {
     private final Path itemModelsDirectory;
     private final Loader fromLoader;
     private final Loader toLoader;
+    private final boolean doLog;
 
-    protected ObjModelsChanger(Path packDirectory, String namespace, Loader fromLoader, Loader toLoader) {
+    protected ObjModelsChanger(Path packDirectory, String namespace, Loader fromLoader, Loader toLoader, boolean doLog) {
         modelsDirectory = packDirectory.resolve("assets").resolve(namespace).resolve("models");
         blockModelsDirectory = modelsDirectory.resolve("block");
         itemModelsDirectory = modelsDirectory.resolve("item");
         this.fromLoader = fromLoader;
         this.toLoader = toLoader;
+        this.doLog = doLog;
     }
 
-    private ObjModelsChanger(Path packDirectory, String namespace, Loader fromLoader) {
+    private ObjModelsChanger(Path packDirectory, String namespace, Loader fromLoader, boolean doLog) {
         modelsDirectory = packDirectory.resolve("assets").resolve(namespace).resolve("models");
         blockModelsDirectory = modelsDirectory.resolve("block");
         itemModelsDirectory = modelsDirectory.resolve("item");
         this.fromLoader = fromLoader;
         this.toLoader = LoaderMain.MOD_LOADER;
+        this.doLog = doLog;
     }
 
     /**
@@ -48,9 +51,10 @@ public class ObjModelsChanger {
      * @param namespace The namespace (the folder) containing the model files. E.g. {@code pack_root/assets/my_namespace <-- this}.
      * @param fromLoader The loader that the models currently have, will be changed.
      * @param toLoader The new loader to change the models loader to.
+     * @param doLog If the models being patched should be logged.
      */
-    public static ObjModelsChanger createNew(Path packDirectory, String namespace, Loader fromLoader, Loader toLoader) {
-        return new ObjModelsChanger(packDirectory, namespace, fromLoader, toLoader);
+    public static ObjModelsChanger createNew(Path packDirectory, String namespace, Loader fromLoader, Loader toLoader, boolean doLog) {
+        return new ObjModelsChanger(packDirectory, namespace, fromLoader, toLoader, doLog);
     }
 
     /**
@@ -58,9 +62,10 @@ public class ObjModelsChanger {
      * @param packDirectory The root directory of the resourcepack (containing the assets folder, icon, etc.).
      * @param namespace The namespace (the folder) containing the model files. E.g. {@code pack_root/assets/my_namespace <-- this}.
      * @param fromLoader The loader that the models currently have, will be changed.
+     * @param doLog If the models being patched should be logged.
      */
-    public static ObjModelsChanger createNew(Path packDirectory, String namespace, Loader fromLoader) {
-        return new ObjModelsChanger(packDirectory, namespace, fromLoader);
+    public static ObjModelsChanger createNew(Path packDirectory, String namespace, Loader fromLoader, boolean doLog) {
+        return new ObjModelsChanger(packDirectory, namespace, fromLoader, doLog);
     }
 
     // Self-explanatory tbh
@@ -75,10 +80,10 @@ public class ObjModelsChanger {
         }
     }
 
-    protected static void changeObjModelLoader(Path modelsDirectory, Path modelFile, Loader fromLoader, Loader toLoader) {
+    protected static void changeObjModelLoader(Path modelsDirectory, Path modelFile, Loader fromLoader, Loader toLoader, boolean log) {
         if (Files.isRegularFile(modelFile)) {  // Stay safe
             try {
-                PL.logI("Patching obj loader, model: '" + modelsDirectory.relativize(modelFile) + "'");
+                if (log) PL.logI("Patching obj loader, model: '" + modelsDirectory.relativize(modelFile) + "'");
                 String modelAsString = Files.readString(modelFile);
                 if (modelAsString.contains("\"" + fromLoader.name + ":obj\"")) {
                     modelAsString = modelAsString.replace("\"" + fromLoader.name + ":obj\"", "\"" + toLoader.name + ":obj\"");
@@ -108,7 +113,7 @@ public class ObjModelsChanger {
                         .filter(Files::isRegularFile)
                         .filter(path -> path.toString().endsWith(".json"))
                         .forEach(modelFile -> {
-                            changeObjModelLoader(modelsDirectory, modelFile, fromLoader, toLoader);
+                            changeObjModelLoader(modelsDirectory, modelFile, fromLoader, toLoader, doLog);
                             changedBlockModels.incrementAndGet();
                         });
             } catch (IOException e) {
@@ -122,7 +127,7 @@ public class ObjModelsChanger {
                         .filter(Files::isRegularFile)
                         .filter(path -> path.toString().endsWith(".json"))
                         .forEach(modelFile -> {
-                            changeObjModelLoader(modelsDirectory, modelFile, fromLoader, toLoader);
+                            changeObjModelLoader(modelsDirectory, modelFile, fromLoader, toLoader, doLog);
                             changedItemModels.incrementAndGet();
                         });
             } catch (IOException e) {
@@ -131,7 +136,7 @@ public class ObjModelsChanger {
         }
 
         if (assetType.isModel()) {
-            PL.logCentered("Loader patching result", PL.line2, true, true);
+            if (doLog) PL.logCentered("Loader patching result", PL.line2, true, true);
             PL.logI("Total models (json): " + (changedBlockModels.get() + changedItemModels.get()));
             PL.logI("Block models: " + changedBlockModels.get());
             PL.logI("Item models: " + changedItemModels.get());
