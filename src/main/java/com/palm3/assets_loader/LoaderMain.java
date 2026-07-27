768db6366bb6108d124c59ffdb66dbf84130acdf
Palm3 <palmerini.2025@gmail.com>
2026-07-27 16:16:09 +0200

    Various, nothing important changed

--- src/main/java/com/palm3/assets_loader/LoaderMain.java
+++ src/main/java/com/palm3/assets_loader/LoaderMain.java
@@ -2,39 +2,47 @@ package com.palm3.assets_loader;
 
 import com.mojang.logging.LogUtils;
 import com.palm3.assets_loader.assets.AssetsLoader;
-import com.palm3.assets_loader.assets.AssetsPatcher;
+import com.palm3.assets_loader.assets.patchers.ModelsChanger;
 import net.minecraft.network.chat.Component;
+import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.IModBusEvent;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.event.lifecycle.ModLifecycleEvent;
 import net.neoforged.neoforge.event.AddPackFindersEvent;
 import org.slf4j.Logger;
-
-import java.util.List;
 
 //todo fix Files.walk() not using try, remove concatenation of / in paths, use resolve
 //todo remove jar files that don't load after first startup
 //todo log decently
 //todo enable/disable assets patching
+//todo move in main the introdutcion/start in asset loader
+//todo add mod name for the one it's loading the assets to know who's using the classes
 
 @Mod(LoaderMain.MOD_ID)
 public class LoaderMain {
-    public static final String MOD_ID = "assets_loader";
     public static final Logger LOGGER = LogUtils.getLogger();
     public static final PrettyLogging.DefaultPrettyLoggingParams DEF_PL_PARAMS = new PrettyLogging.DefaultPrettyLoggingParams(LOGGER, "-", 83, "-", 41);
-    public static final PrettyLogging PL = new PrettyLogging(LOGGER, DEF_PL_PARAMS);
 
+    // Mod infos
+    public static final String MOD_ID = "assets_loader";
     public static final String MOD_VERSION = "1.0.0";
     public static final String DISCORD_LINK = "https://discord.com/invite/BuMv2f8epp";
+    public static final ModelsChanger.Loader MOD_LOADER = ModelsChanger.Loader.NEOFORGE;
 
     public LoaderMain(IEventBus modEventBus) {
         modEventBus.addListener(LoaderMain::packFindersEvent);
         AssetsInGameTest.BLOCKS.register(modEventBus);
     }
 
+    public static final AssetsLoader assetsLoader = new AssetsLoader(".temp_assets");
+
     @SubscribeEvent
     public static void packFindersEvent(AddPackFindersEvent event) {
-        AssetsLoader assetsLoader = new AssetsLoader(".temp_assets");
+
 
         assetsLoader.single.loadAssetsCustomFolderName(
                 event,
@@ -47,10 +55,10 @@ public class LoaderMain {
                 Component.literal("description"),
                 false,
                 true,
-                false
+                true
         );
 
-        assetsLoader.multiple.loadAssets(
+        /*assetsLoader.multiple.loadAssets(
                 event,
                 List.of("Create-Dreams-n-Desires-1.19.2-0.2.5b.PREBETA.jar", "design_decor-0.4.0b-1.20.1.jar"),
                 List.of("create_dd", "design_decor"),
@@ -63,6 +71,6 @@ public class LoaderMain {
                 false,
                 false
 
-        );
+        );*/
     }
 }
