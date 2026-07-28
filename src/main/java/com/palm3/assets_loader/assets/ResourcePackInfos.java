61817cb56a722065acb5461d7e31d6101a3aaf80
Palm3 <palmerini.2025@gmail.com>
2026-07-28 11:43:39 +0200

    Added packFolderName

--- src/main/java/com/palm3/assets_loader/assets/ResourcePackInfos.java
+++ src/main/java/com/palm3/assets_loader/assets/ResourcePackInfos.java
@@ -2,5 +2,5 @@ package com.palm3.assets_loader.assets;
 
 import net.minecraft.network.chat.Component;
 
-public record ResourcePackInfos(Component packTitle, Component packDescription, boolean requiredPack, boolean deletePack) {
+public record ResourcePackInfos(String packFolderName, Component packTitle, Component packDescription, boolean requiredPack, boolean deletePack) {
 }
