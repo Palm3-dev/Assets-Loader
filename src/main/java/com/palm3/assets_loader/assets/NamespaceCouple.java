8079430efd60384046be7daa3fb0c4181608fb65
Palm3 <palmerini.2025@gmail.com>
2026-07-28 11:14:51 +0200

    Added single namespace couple List creation method

--- src/main/java/com/palm3/assets_loader/assets/NamespaceCouple.java
+++ src/main/java/com/palm3/assets_loader/assets/NamespaceCouple.java
@@ -1,5 +1,6 @@
 package com.palm3.assets_loader.assets;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,7 +37,8 @@ public record NamespaceCouple(String oldNamespace, String newNamespace) {
      * @return The {@link List} of namespaces couples.
      * @throws IllegalArgumentException If the lists are different in dimension.
      */
-    public static List<NamespaceCouple> createNamespacesList(List<String> oldNamespaces, List<String> newNamespaces) throws IllegalArgumentException {
+    @ParametersAreNonnullByDefault
+    public static List<NamespaceCouple> createMultipleNamespacesList(List<String> oldNamespaces, List<String> newNamespaces) throws IllegalArgumentException {
         // Check lists or throw
         if (oldNamespaces.size() != newNamespaces.size()) {
             String biggerList = oldNamespaces.size() > newNamespaces.size() ? "old_namespaces" : "new_namespaces";
@@ -49,6 +51,19 @@ public record NamespaceCouple(String oldNamespace, String newNamespace) {
             namespacesCouples.add(new NamespaceCouple(oldNamespace, newNamespaces.get(newIndex)));
             newIndex++;
         }
+        return namespacesCouples;
+    }
+
+    /**
+     * Creates a list of one {@link NamespaceCouple}.
+     * @param oldNamespace The old namespace.
+     * @param newNamespace The new namespace.
+     * @return The {@link List} of namespaces couples.
+     */
+    @ParametersAreNonnullByDefault
+    public static List<NamespaceCouple> createSingleNamespacesList(String oldNamespace, String newNamespace) throws IllegalArgumentException {
+        List<NamespaceCouple> namespacesCouples = new ArrayList<>();
+        namespacesCouples.add(new NamespaceCouple(oldNamespace, newNamespace));
         return namespacesCouples;
     }
 }
