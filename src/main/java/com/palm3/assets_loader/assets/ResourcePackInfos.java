16b444336cf0d58220aea03546199f000384d8ed
Palm3 <palmerini.2025@gmail.com>
2026-08-04 15:49:38 +0200

    Javadoc

--- src/main/java/com/palm3/assets_loader/assets/ResourcePackInfos.java
+++ src/main/java/com/palm3/assets_loader/assets/ResourcePackInfos.java
@@ -6,7 +6,7 @@ import net.minecraft.network.chat.Component;
  * Holds the final resourcepack infos.
  * @param packFolderName The name of the resourcepack folder root (containing the {@code assets} folder, {@code pack.png} etc...).
  * @param packTitle The in-game title of the resourcepack.
- * @param packDescription The in-gam,e description of the resourcepack.
+ * @param packDescription The in-game description of the resourcepack.
  * @param requiredPack If the pack is required and thus cannot be disabled in-game.
  * @param deletePack If the pack should be deleted after the game is closed.
  */
