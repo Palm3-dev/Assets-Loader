c9cf56504668fd56081765fdcb40010362b50439
Palm3 <palmerini.2025@gmail.com>
2026-08-05 17:21:29 +0200

    Added record to do things on game closing/crash

---
+++ src/main/java/com/palm3/packs_loader/common/GameClosedTask.java
@@ -0,0 +1,50 @@
+package com.palm3.packs_loader.common;
+
+import com.mojang.logging.LogUtils;
+import com.palm3.packs_loader.PacksLoaderMain;
+import com.palm3.packs_loader.logging.PrettyLogging;
+
+/**
+ * This record is used to add a shutdown task to Minecraft.
+ * @param task The task you want to execute.
+ * @param executeForCrashes If the task should be also executed when the game crashes.
+ * @param taskName The name of the task, to identify it. Giving reasonable and understandable names is recommended.
+ */
+public record GameClosedTask(Runnable task, boolean executeForCrashes, String taskName) {
+
+    private static final PrettyLogging PL = new PrettyLogging(LogUtils.getLogger(), PacksLoaderMain.DEF_PL_PARAMS);
+
+    /**
+     * This record is used to add a shutdown task to Minecraft.
+     * @param task The task you want to execute.
+     * @param executeForCrashes If the task should be also executed when the game crashes.
+     * @param taskName The name of the task, to identify it. Giving reasonable and understandable names is recommended.
+     */
+    public GameClosedTask(Runnable task, boolean executeForCrashes, String taskName) {
+        this.task = task;
+        this.executeForCrashes = executeForCrashes;
+        this.taskName = taskName;
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            PL.logI("Executing shutdown task: " + taskName);
+            task.run();
+        }));
+
+        if (executeForCrashes) {
+            Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+                PL.logW("Oops,something exploded! Executing shutdown-designated task: " + taskName);
+                task.run();
+            });
+        }
+    }
+
+    /**
+     * This record is used to add a shutdown task to Minecraft.
+     * @param task The task you want to execute.
+     * @param executeForCrashes If the task should be also executed when the game crashes.
+     * @param taskName The name of the task, to identify it. Giving reasonable and understandable names is recommended.
+     */
+    public static GameClosedTask create(Runnable task, boolean executeForCrashes, String taskName) {
+        return new GameClosedTask(task, executeForCrashes, taskName);
+    }
+}
