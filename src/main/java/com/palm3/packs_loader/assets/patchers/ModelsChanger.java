8db8e118737e20e9dfe839506735579261611df4
Palm3 <palmerini.2025@gmail.com>
2026-08-05 09:36:44 +0200

    Made ModelsChanger utility class

--- src/main/java/com/palm3/packs_loader/assets/patchers/ModelsChanger.java
+++ src/main/java/com/palm3/packs_loader/assets/patchers/ModelsChanger.java
@@ -4,63 +4,27 @@ import com.mojang.logging.LogUtils;
 import com.palm3.packs_loader.PacksLoaderMain;
 import com.palm3.packs_loader.ModLoader;
 import com.palm3.packs_loader.PrettyLogging;
+import org.apache.logging.log4j.core.util.Loader;
 
 import java.io.IOException;
+import java.nio.file.AtomicMoveNotSupportedException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.EnumSet;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 /**
- * This class is used to patch minecraft models coming from different loaders or versions.
+ * This utility class offers various statics to patch minecraft models coming from different loaders or versions.
  * <br>It offers an obj model changer method for the loader difference.
- * <br>It also offers a static method {@link ModelsChanger#changeJsonModelString(Path, String, String, QuotationMarkPos)}
+ * <br>It also offers the method {@link ModelsChanger#changeJsonModelString(Path, String, String, QuotationMarkPos)}
  * that can be used to make custom changes to specific models not covered here.
  */
 public class ModelsChanger {
 
     private static final PrettyLogging PL = new PrettyLogging(LogUtils.getLogger(), PacksLoaderMain.DEF_PL_PARAMS);
-    private final Path modelsDirectory;
-    private final ModLoader fromLoader;
-    private final ModLoader toLoader;
-
-    protected ModelsChanger(Path packDirectory, String namespace, ModLoader fromLoader, ModLoader toLoader) {
-        modelsDirectory = packDirectory.resolve("assets").resolve(namespace).resolve("models");
-        this.fromLoader = fromLoader;
-        this.toLoader = toLoader;
-    }
-
-    /**
-     * Create an instance of {@link ModelsChanger}, can specify the final loader.
-     * @param packDirectory The root directory of the resourcepack (containing the assets folder, icon, etc.).
-     * @param namespace The namespace (the folder) containing the model files. E.g. {@code pack_root/assets/my_namespace <-- this}.
-     * @param fromLoader The loader that the models currently have, will be changed.
-     * @param toLoader The new loader to change the models loader to.
-     */
-    public static ModelsChanger createNew(Path packDirectory, String namespace, ModLoader fromLoader, ModLoader toLoader) {
-        return new ModelsChanger(packDirectory, namespace, fromLoader, toLoader);
-    }
-
-    /**
-     * Create an instance of {@link ModelsChanger}. The final loader is the one of this mod.
-     * @param packDirectory The root directory of the resourcepack (containing the assets folder, icon, etc.).
-     * @param namespace The namespace (the folder) containing the model files. E.g. {@code pack_root/assets/my_namespace <-- this}.
-     * @param fromLoader The loader that the models currently have, will be changed.
-     */
-    public static ModelsChanger createNew(Path packDirectory, String namespace, ModLoader fromLoader) {
-        return new ModelsChanger(packDirectory, namespace, fromLoader, PacksLoaderMain.MOD_LOADER);
-    }
-
-    /// @return The {@link Path} of the item models directory.
-    public Path getItemModelsDir() {
-        return modelsDirectory.resolve("item");
-    }
-
-    /// @return The {@link Path} of the block models directory.
-    public Path getBlockModelsDir() {
-        return modelsDirectory.resolve("block");
-    }
 
     /// Indicates the position of the quotation marks in a json string.
     public enum QuotationMarkPos {
@@ -86,8 +50,11 @@ public class ModelsChanger {
      * @param oldString The old string.
      * @param newString The new string that will replace the old one.
      * @param quotationMarksPos The position of the quotation marks in the strings, can be disabled with {@link QuotationMarkPos#NONE}.
+     * @return {@code true} if the model has been changed successfully, {@code false} if the model hasn't been changed, exceptions happened or others.
+     *         You can ignore the return value most of the time.
      */
-    public static void changeJsonModelString(Path modelFile, String oldString, String newString, QuotationMarkPos quotationMarksPos) {
+    public static boolean changeJsonModelString(Path modelFile, String oldString, String newString, QuotationMarkPos quotationMarksPos) {
+        boolean modelsChanged = false;
         if (Files.isRegularFile(modelFile) && modelFile.toString().endsWith(".json")) {
             try {
                 String oldJsonStr = oldString;
@@ -106,6 +73,7 @@ public class ModelsChanger {
                     PL.logI("Changing json value " + oldJsonStr + " to " + newJsonStr + ", model file: " + modelFile.getFileName());
                     jsonAsString = jsonAsString.replace(oldJsonStr, newJsonStr);
                     Files.writeString(modelFile, jsonAsString);
+                    modelsChanged = true;
                 }
             } catch (IOException e) {
                 PL.logE("Exception caught during json file " + modelFile.getFileName() + " read/write: " + e);
@@ -113,58 +81,109 @@ public class ModelsChanger {
         } else {
             PL.logI("Tried to change a json string of a non-json file: '" + modelFile.getFileName() + "'");
         }
+        return modelsChanged;
     }
+
+    /**
+     * Record holds a couple of loaders used to define from which loader the models come from and to which loader change them to.
+     * @param fromLoader The old models loader.
+     * @param toLoader The new models loader.
+     */
+    public record LoaderCouple(ModLoader fromLoader, ModLoader toLoader) {}
 
     /**
      * Used to patch {@code .obj} block models. If the {@code .json} model, referencing an {@code .obj}
      * model comes from a different loader, it has the json key {@code "loader"} with value {@code <loader>:obj}.
      * The wrong loader causes the model to fail the loading in-game. This is used to fix that.
-     * @param assetType The type of model (all, item, block).
+     * @param assetType The type of model you want to change (see {@link AssetType}).
+     *                  If the type is not a model you'll get a WARN with an {@link IllegalArgumentException} (without crashes).
+     * @param packRoot The root of the resourcepack (folder containing the icon, the assets folder, etc...).
+     * @param loaders The loaders of the pack, old and new one. See {@link LoaderCouple}.
+     * @param namespaces A list of namespaces where to patch the models.
      */
-    public void changeModelsObjLoader(AssetType assetType) {
+    public static void changeModelsObjLoader(AssetType assetType, Path packRoot, LoaderCouple loaders, List<String> namespaces) {
         if (!assetType.isModel()) {
-            PL.logW("Tried to change obj models, but specified change is " + assetType + ", skipping.");
+            PL.logW(new IllegalArgumentException("Tried to change obj models, but specified change is " + assetType + ", skipping.").toString());
             return;
         }
 
-        PL.logSpaceI();
-        PL.logCenteredI("Patching " + fromLoader.name + " obj models loader", PL.line2, true, true);
+        PL.logCenteredI("Patching " + loaders.fromLoader + " obj models loader", PL.line1);
 
-        AtomicInteger itemModels = new AtomicInteger();
-        AtomicInteger blockModels = new AtomicInteger();
+        namespaces.forEach(namespace -> {
+            PL.logI("Models namespace: '" + namespace + "'");
 
-        if (assetType.isBlockModel()) {
-            try (Stream<Path> modelsStream = Files.walk(getBlockModelsDir())) {
-                modelsStream
-                        .filter(Files::isRegularFile)
-                        .filter(path -> path.toString().endsWith(".json"))
-                        .forEach(modelFile -> {
-                            changeJsonModelString(modelFile, fromLoader.name + ":obj", toLoader.name + ":obj", QuotationMarkPos.BOTH);
-                            blockModels.incrementAndGet();
-                        });
-            } catch (IOException e) {
-                PL.logE("Exception caught during block models files walk: " + e);
+            AtomicInteger totalItemModels = new AtomicInteger();
+            AtomicInteger totalBlockModels = new AtomicInteger();
+            AtomicInteger changedItemModels = new AtomicInteger();
+            AtomicInteger changedBlockModels = new AtomicInteger();
+
+            Path modelsPath = getModelsDir(packRoot, namespace);
+
+            if (assetType.isBlockModel()) {
+                try (Stream<Path> modelsStream = Files.walk(getBlockModelsDir(modelsPath))) {
+                    modelsStream
+                            .filter(Files::isRegularFile)
+                            .filter(path -> path.toString().endsWith(".json"))
+                            .forEach(modelFile -> {
+                                if (changeJsonModelString(modelFile, loaders.fromLoader.name + ":obj", loaders.toLoader.name + ":obj", QuotationMarkPos.BOTH))
+                                    changedBlockModels.incrementAndGet();
+                                totalBlockModels.incrementAndGet();
+                            });
+                } catch (IOException e) {
+                    PL.logE("Exception caught during block models files walk: " + e);
+                }
             }
-        }
 
-        if (assetType.isItemModel()) {
-            try (Stream<Path> modelsStream = Files.walk(getItemModelsDir())) {
-                modelsStream
-                        .filter(Files::isRegularFile)
-                        .filter(path -> path.toString().endsWith(".json"))
-                        .forEach(modelFile -> {
-                            changeJsonModelString(modelFile, fromLoader.name + ":obj", toLoader.name + ":obj", QuotationMarkPos.BOTH);
-                            itemModels.incrementAndGet();
-                        });
-            } catch (IOException e) {
-                PL.logE("Exception caught during item models files walk: " + e);
+            if (assetType.isItemModel()) {
+                try (Stream<Path> modelsStream = Files.walk(getItemModelsDir(modelsPath))) {
+                    modelsStream
+                            .filter(Files::isRegularFile)
+                            .filter(path -> path.toString().endsWith(".json"))
+                            .forEach(modelFile -> {
+                                if (changeJsonModelString(modelFile, loaders.fromLoader.name + ":obj", loaders.toLoader.name + ":obj", QuotationMarkPos.BOTH))
+                                    changedItemModels.incrementAndGet();
+                                totalItemModels.incrementAndGet();
+                            });
+                } catch (IOException e) {
+                    PL.logE("Exception caught during item models files walk: " + e);
+                }
             }
-        }
 
-        PL.logCenteredI("Loader patching result", PL.line2, true, true);
-        PL.logI("Total models (json): " + (blockModels.get() + itemModels.get()));
-        PL.logI("Block models: " + blockModels.get());
-        PL.logI("Item models: " + itemModels.get());
-        PL.logI(PL.line2);
+            PL.logCenteredI("Loader patching result", PL.line2);
+            PL.logI("Total models: " + (totalItemModels.get() + totalBlockModels.get()));
+            PL.logI("Total block models: " + totalBlockModels.get());
+            PL.logI("Total item models: " + totalItemModels.get());
+            PL.logI("Patched block models: " + changedBlockModels.get());
+            PL.logI("Patched item models: " + changedItemModels.get());
+            PL.logI(PL.line2);
+        });
+    }
+
+    /**
+     * Gets you the models directory.
+     * @param packRoot The root of your resourcepack.
+     * @param namespace The namespace of the models inside the resourcepack.
+     * @return The {@link Path} of the models directory.
+     */
+    public static Path getModelsDir(Path packRoot, String namespace) {
+        return packRoot.resolve("assets").resolve(namespace).resolve("models");
+    }
+
+    /**
+     * Gets you the item models directory.
+     * @param modelsPath The models directory in your pack namespace.
+     * @return The {@link Path} of the item models directory.
+     */
+    public static Path getItemModelsDir(Path modelsPath) {
+        return modelsPath.resolve("item");
+    }
+
+    /**
+     * Gets you the block models directory.
+     * @param modelsPath The models directory in your pack namespace.
+     * @return The {@link Path} of the block models directory.
+     */
+    public static Path getBlockModelsDir(Path modelsPath) {
+        return modelsPath.resolve("block");
     }
 }
