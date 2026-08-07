745d6ef5b7984a63d7176844f8de63e3136bf629
Palm3 <palmerini.2025@gmail.com>
2026-08-07 12:07:26 +0200

    Added PackType mc conversion method

--- src/main/java/com/palm3/packs_loader/common/PackType.java
+++ src/main/java/com/palm3/packs_loader/common/PackType.java
@@ -24,7 +24,26 @@ public enum PackType {
     /**
      * @return the folder name of the corresponding pack type. {@code "both"} if the enum value is {@link PackType#BOTH}.
      */
-    public @Nullable String folderName() {
+    public String folderName() {
         return folderName;
+    }
+
+    /**
+     * Converts this pack type to the minecraft one {@link net.minecraft.server.packs.PackType}.
+     */
+    public net.minecraft.server.packs.PackType toMcPackType() {
+        switch (this) {
+            case ASSETS -> {
+                return net.minecraft.server.packs.PackType.CLIENT_RESOURCES;
+            }
+
+            case DATAPACK -> {
+                return net.minecraft.server.packs.PackType.SERVER_DATA;
+            }
+
+            case BOTH -> {
+                return null;
+            }
+        }
     }
 }
